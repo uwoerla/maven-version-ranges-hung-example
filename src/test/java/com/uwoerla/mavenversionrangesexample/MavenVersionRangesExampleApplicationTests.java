615555b286299ee58f83b37185670f5ad0f4f2d8
uwoerla <uwoerla@gmail.com>
2023-03-10 12:54:52 +0800

    first commit

---
+++ src/test/java/com/uwoerla/mavenversionrangesexample/MavenVersionRangesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwoerla.mavenversionrangesexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenVersionRangesExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
